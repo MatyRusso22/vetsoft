d259b83e25368daf8d7f9dfc1ef33be0b1732449
DiegoSivori <sivori.d@gmail.com>
2024-06-04 23:52:55 -0300

    Correccion de las vistas de veterinario

--- app/views.py
+++ app/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
-
 from .forms import PetForm
 from .models import Client, Medicine, Pet, Product, Provider, Vet
-
 
 def home(request):
     """
@@ -11,14 +9,12 @@ def home(request):
     """
     return render(request, "home.html")
 
-
 def clients_repository(request):
     """
     Muestra el repositorio de clientes.
     """
     clients = Client.objects.all()
     return render(request, "clients/repository.html", {"clients": clients})
-
 
 def clients_form(request, id=None):
     """
@@ -34,10 +30,8 @@ def clients_form(request, id=None):
         else:
             client = get_object_or_404(Client, pk=client_id)
             client.update_client(request.POST)
-
         if saved:
             return redirect(reverse("clients_repo"))
-
         return render(
             request, "clients/form.html", {"errors": errors, "client": request.POST}
         )
@@ -47,7 +41,6 @@ def clients_form(request, id=None):
         client = get_object_or_404(Client, pk=id)
 
     return render(request, "clients/form.html", {"client": client})
-
 
 def clients_delete(request):
     """
@@ -86,15 +79,12 @@ def pets_form(request, id=None):
             except ValueError as e:
                 errors["weight"] = str(e)
                 saved = False
-
         if saved:
             return redirect(reverse("pets_repo"))
     else:
         if id is not None:
             pet = get_object_or_404(Pet, pk=id)
-
     form = PetForm(request.POST or None, instance=pet)
-
     return render(
         request, "pets/form.html", {"errors": errors, "form": form, "form_title": "Agregar Mascota", "form_action": "pets_form"}
     )
@@ -107,7 +97,6 @@ def pets_delete(request):
     pet = get_object_or_404(Pet, pk=int(pet_id))
     pet.delete()
     return redirect(reverse("pets_repo"))
-
 
 def medicines_repository(request):
     """
@@ -126,24 +115,19 @@ def medicines_form(request, id=None):
         medicine = None
         medicine_id = request.POST.get("id", "") 
         
-
         if medicine_id == "":
             saved, errors = Medicine.save_medicine(request.POST)
         else:
             medicine = get_object_or_404(Medicine, pk=medicine_id)
             medicine.update_medicine(request.POST)
-
         if saved:
             return redirect(reverse("medicines_repo"))
-
         return render (request, "medicines/form.html", {"errors": errors, "medicines": request.POST})
     
     medicine=None
     if id is not None: 
-        medicine=get_object_or_404(Medicine, pk=id)
-        
+        medicine=get_object_or_404(Medicine, pk=id) 
     return render (request, "medicines/form.html", { "medicines": medicine})
-
 
 def medicines_delete(request):
     """
@@ -182,15 +166,11 @@ def provider_form(request, id=None):
         return render(
             request, "provider/form.html", {"errors": errors, "provider": request.POST}
         )
-
     provider = None
     if id is not None:
         provider = get_object_or_404(Provider, pk=id)
 
     return render(request, "provider/form.html", {"provider": provider})
-    errors = {}
-    provider = None
-
 
 def provider_delete(request):
     """
@@ -208,7 +188,6 @@ def products_repository(request):
     products = Product.objects.all()
     return render(request, "products/repository.html", {"products": products})
 
-
 def products_form(request, id=None):
     """
     Maneja el formulario para crear o actualizar un producto.
@@ -223,10 +202,8 @@ def products_form(request, id=None):
         else:
             product = get_object_or_404(Product, pk=product_id)
             saved, errors = product.update_product(request.POST)
-
         if saved:
             return redirect(reverse("products_repo"))
-
         return render(
             request, "products/form.html", {"errors": errors, "product": request.POST}
         )
@@ -234,7 +211,6 @@ def products_form(request, id=None):
     product = None
     if id is not None:
         product = get_object_or_404(Product, pk=id)
-
     return render(request, "products/form.html", {"product": product})
 
 def products_delete(request):
@@ -244,21 +220,20 @@ def products_delete(request):
     product_id = request.POST.get("product_id")
     product = get_object_or_404(Product, pk=int(product_id))
     product.delete()
-
     return redirect(reverse("products_repo"))
 
 def vet_repository(request):
     """
     Muestra el repositorio de veterinarios.
     """
-    vet = Vet.objects.all()
-    return render(request, "vet/repository.html", {"vet": vet})
-
+    vets = Vet.objects.all()
+    return render(request, "vet/repository.html", {"vets": vets})
 
 def vet_form(request, id=None):
     """
     Maneja el formulario para crear o actualizar un veterinario.
     """
+    specialities = Vet.SPECIALITY_CHOICES.choices
     if request.method == "POST":
         vet_id = request.POST.get("id", "")
         errors = {}
@@ -268,20 +243,19 @@ def vet_form(request, id=None):
             saved, errors = Vet.save_vet(request.POST)
         else:
             vet = get_object_or_404(Vet, pk=vet_id)
-            vet.update_vet(request.POST)
+            saved, errors= vet.update_vet(request.POST)
 
         if saved:
             return redirect(reverse("vet_repo"))
 
         return render(
-            request, "vet/form.html", {"errors": errors, "vet": request.POST}
+            request, "vet/form.html", {"errors": errors, "vet": request.POST, "specialities" : specialities}
         )
 
     vet = None
     if id is not None:
-        vet = get_object_or_404(Vet, pk=id) 
-    return render(request, "vet/form.html", {"vet": vet})
-
+        vet = get_object_or_404(Vet, pk=id)
+    return render(request, "vet/form.html", {"vet": vet, "specialities" : specialities})
 
 def vet_delete(request):
     """
@@ -290,5 +264,4 @@ def vet_delete(request):
     vet_id = request.POST.get("vet_id")
     vet = get_object_or_404(Vet, pk=int(vet_id))
     vet.delete()
-
     return redirect(reverse("vet_repo"))
