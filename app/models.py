abd3e2fc25b9cd5ce3ea46bf92963683a14565f8
Mauro Baroni <maurojavier_laplata@hotmail.com>
2024-06-05 16:29:43 -0300

    Agrego validacion en el telefono de cliente para que comience con 54

--- app/models.py
+++ app/models.py
@@ -17,9 +17,13 @@ def validate_client(data):
         errors["phone"] = "Por favor ingrese un teléfono"
     elif not phone.isdigit():
         errors["phone"] = "Por favor ingrese un telefono valido"
-
-    elif not str(phone).startswith("54"):
-            errors["phone"] = "El teléfono debe comenzar con 54"
+    else: 
+        try:
+            number = int(phone[:2])
+            if number != 54:
+                errors['phone'] = "El teléfono debe comenzar con 54" 
+        except ValueError:
+            errors["phone"] = "Por favor ingrese un telefono valido"
             
     if email == "":
         errors["email"] = "Por favor ingrese un email"
